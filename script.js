3e76f6ed3495aefd6832ee5520c7d1d535636682
Gun Agrawal <gunagrawal0412@gmail.com>
2026-09-12 05:11:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ import {
 
 const firebaseConfig = {
     apiKey: "AIzaSyAikl4RKUs6L31vAnoNTEj7Q0nSlgydQz4",
-    authDomain: authDomain: "pasteldiary-h8m3pxx66-codewithgun04s-projects.vercel.app",,
+    authDomain: "pasteldiary-h8m3pxx66-codewithgun04s-projects.vercel.app",
     projectId: "pasteldiary-61e12",
     storageBucket: "pasteldiary-61e12.firebasestorage.app",
     messagingSenderId: "615784663317",
